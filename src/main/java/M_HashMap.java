c51c17fd00e7aa8714bb7d5fb364205ebe940cdd
duxiaoyan24 <duxiaoyan24@sina.com>
2016-10-24 11:19:27 +0800

    init

---
+++ src/main/java/M_HashMap.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by BFD-308 on 2016/10/24.
+ */
+public class M_HashMap {
+    public static void main(String[] arg) {
+    }
+
+    public static void Function1() {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+        }
+    }
+
+    public static void Function2() {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        //遍历map中的键
+        for (Integer key : map.keySet()) {
+            System.out.println("Key = " + key);
+        }
+        //遍历map中的值
+        for (Integer value : map.values()) {
+            System.out.println("Value = " + value);
+        }
+    }
+
+    public static void Function3() {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (Integer key : map.keySet()) {
+            // 从键取值是耗时的
+            Integer value = map.get(key);
+            System.out.println("Key = " + key + ", Value = " + value);
+        }
+    }
+}
